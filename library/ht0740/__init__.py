2e26e61e211255659185592b9cb2d09b569e8900
Phil Howard <phil@gadgetoid.com>
2019-09-13 10:55:56 +0100

    Bump version

--- library/ht0740/__init__.py
+++ library/ht0740/__init__.py
@@ -1,6 +1,6 @@
 from i2cdevice import Device, Register, BitField
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 
 class IOItem:
--- library/setup.py
+++ library/setup.py
@@ -39,7 +39,7 @@ classifiers = ['Development Status :: 4 - Beta',
 
 setup(
     name='ht0740',
-    version='0.0.1',
+    version='0.0.2',
     author='Gee Bartlett',
     author_email='gee@pimoroni.com',
     description="""Python library for the HT0740 Switch""",
